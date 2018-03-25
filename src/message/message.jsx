e89a6311782ff1a2723ff1096b2bff48f1b99cf7
gage langdon <gagelangdon@gmail.com>
2018-03-25 15:03:43 -0700

    feat: add support for optional heading on message component

--- src/message/message.jsx
+++ src/message/message.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Message = ({ message, isSuccess, isInfo }) => {
+const Message = ({ heading, message, isSuccess, isInfo }) => {
 	return (
 		<div>
 			{message ?
 				<div
 					className={`mt-2 alert alert-${(isInfo ? 'info' : isSuccess ? 'success' : 'danger')}`}
 					role="alert">
-					{!isInfo ? <strong>{isSuccess ? 'Awesome!' : 'Oops!'} </strong> : null}
+					{!isInfo ? <strong>{heading ? heading : isSuccess ? 'Awesome!' : 'Oops!'} </strong> : null}
 					{message}
 				</div> : null
 			}
@@ -19,5 +19,6 @@ Message.propTypes = {
 	message: PropTypes.string,
 	isSuccess: PropTypes.bool,
 	isInfo: PropTypes.bool,
+	heading: PropTypes.string
 };
 export default Message;
